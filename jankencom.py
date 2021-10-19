bc4b907b9e110e8acb574acb099eeadda6a18905
Tatsuhiko Kimita <nannbo@crappo.net>
2021-10-19 09:51:21 +0900

    Python練習のために、Enumと三項演算子を使った書き方に変えてみた

--- jankencom.py
+++ jankencom.py
@@ -1,57 +1,67 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+# !/usr/bin/env python
 
-import sys
 import random
+from enum import Enum, unique
 
-def janken(dic, user):
-    user_choice = dic[user]
-    choice_list = ['1','2','3']
-    pc = dic[random.choice(choice_list)]
+
+@unique
+class Result(Enum):
     draw = 'DRAW'
     win = 'You Win!!'
     lose = 'You lose!!'
 
-    if user_choice == pc:
-        judge = draw
-    else:
-        if user_choice == "グー":
-            if pc == "チョキ":
-                judge = win
-            else:
-                judge = lose
 
-        elif user_choice == "チョキ":
-            if pc == "パー":
-                judge = win
-            else:
-                judge = lose
+@unique
+class Hands(Enum):
+    グー = '1'
+    チョキ = '2'
+    パー = '3'
+
+    @classmethod
+    def get_random(cls):
+        values = list(map(lambda c: c.value, cls))
+        value = random.choice(values)
+        return Hands(value)
+
+
+def janken(user_input):
+
+    user_hand: Hands = Hands(user_input)
+
+    pc_hand: Hands = Hands.get_random()
+
+    judge: Result
+
+    if user_hand is pc_hand:
+        judge = Result.draw
+    else:
+        if user_hand is Hands.グー:
+            judge = Result.win if (pc_hand == Hands.チョキ) else Result.lose
+
+        elif user_hand is Hands.チョキ:
+            judge = Result.win if (pc_hand == Hands.パー) else Result.lose
 
         else:
-            if pc == u"グー":
-                judge = win
-            else:
-                judge = lose
+            judge = Result.win if (pc_hand == Hands.グー) else Result.lose
 
-    print(u"あなた選んだのは %s"%user_choice)
-    print(u"コンピュータが選んだのは %s"%pc)
-    print(u"結果は%s"%judge)
+    print(u"あなたが選んだのは %s" % user_hand.name)
+    print(u"コンピュータが選んだのは %s" % pc_hand.name)
+    print(u"結果は%s\n"%judge.value)
 
     return judge
 
 
 user_win = 0
 
-
-dic = {"1":"グー","2":"チョキ","3":"パー"}
 for i in range(3):
     print (u"じゃーんけーん")
     print (u"1=グー　2=チョキ　3=パー　を入力")
     user = input('>>>  ')
 
     try:
-        result = janken(dic, user)
-        if result == 'You Win!!':
+        result: Result = janken(user)
+        if result == Result.win:
             user_win += 1
     except:
         print(u"1か２か３を入力してください。")
